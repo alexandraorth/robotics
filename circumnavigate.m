a28c4684d6d63f8f80f99ef5bc71956e594b315a
Tony Ling <tonyling@users.noreply.github.com>
2014-09-16 19:36:15 -0400

    Update circumnavigate.m

--- circumnavigate.m
+++ circumnavigate.m
@@ -31,6 +31,36 @@ function finished= nav(serPort)
     circled= 0;
     finished= 0;
     SetFwdVelAngVelCreate(serPort, 0, 0); %stop the robot
+    wallSensor= WallSensorReadRoomba(serPort);
+    display(wallSensor);
+    %if not detecting wall, turn left until wall detected
+    if wallSensor == 0
+        while wallSensor == 0
+            turnAngle(serPort, .2, 5); %turn 5 degree increments
+            pause(.5);
+            wallSensor = WallSensorReadRoomba(serPort);
+            display(wallSensor);
+        end    
+    end
+    pause(1);
+    ping= 1;
+    display(wallSensor);
+    display(ping);
+    %robot should be detecting wall by this point
+    while wallSensor > 0
+        display(wallSensor);
+        turnAngle(serPort, .2, 5); %turn 5 degree increments
+        wallSensor = WallSensorReadRoomba(serPort);
+    end
+    display(ping);
+    display(wallSensor);
+    %robot not detecting wall anymore
+    while wallSensor == 0
+        turnAngle(serPort, .2, -5);
+        wallSensor =  WallSensorReadRoomba(serPort);
+    end
+    display(ping);
+    display(wallSensor);    
     while circled < 1 
         pause(0.1);
     end
