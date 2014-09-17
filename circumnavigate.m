daa2240fb2ae9a57adde40cecdba289e75c8dc94
Alexandra Orth <alexandra.orth@me.com>
2014-09-16 22:34:50 -0400

    basic circumnavigation with bump left and bump right

--- circumnavigate.m
+++ circumnavigate.m
@@ -6,7 +6,7 @@ function pos= circumnavigate(serPort)
     while pos < 1
         frontBump= checkFrontBump(serPort);
         if frontBump %if front bump, this triggers into circumnavigation mode
-            nav(serPort);
+           nav(serPort);
         end
         pause(0.01);
     end
@@ -32,36 +32,44 @@ function finished= nav(serPort)
     finished= 0;
     SetFwdVelAngVelCreate(serPort, 0, 0); %stop the robot
     wallSensor= WallSensorReadRoomba(serPort);
-    display(wallSensor);
+
+    fprintf('Turning left until wall detected\n');
     %if not detecting wall, turn left until wall detected
     if wallSensor == 0
         while wallSensor == 0
             turnAngle(serPort, .2, 5); %turn 5 degree increments
             pause(.5);
             wallSensor = WallSensorReadRoomba(serPort);
-            display(wallSensor);
         end    
     end
     pause(1);
-    ping= 1;
-    display(wallSensor);
-    display(ping);
-    %robot should be detecting wall by this point
-    while wallSensor > 0
-        display(wallSensor);
-        turnAngle(serPort, .2, 5); %turn 5 degree increments
-        wallSensor = WallSensorReadRoomba(serPort);
-    end
-    display(ping);
-    display(wallSensor);
-    %robot not detecting wall anymore
-    while wallSensor == 0
-        turnAngle(serPort, .2, -5);
-        wallSensor =  WallSensorReadRoomba(serPort);
-    end
-    display(ping);
-    display(wallSensor);    
-    while circled < 1 
-        pause(0.1);
+
+
+    fprintf('Robot is detecting wall- will begin circumnavigating\n');
+%     circumnavigation loop
+    i = 1;
+    while i == 1%need condition- this will eventually be the starting pt
+        [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
+        BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
+    
+        if BumpLeft
+            turnAngle(serPort, .2, 90);
+        end
+        if BumpRight || BumpFront
+          turnAngle(serPort, .2, 50);
+        end
+        SetFwdVelAngVelCreate(serPort, .4, -.4);
+        pause(.5);
+        
+%         if wallSensor ==  0
+%            if BumpFront
+%                turnAngle(serPort, .2, 45);
+%            end
+%            SetFwdVelAngVelCreate(serPort, .4, -.4);   
+%         else
+%             turnAngle(serPort, .2, 20); %turn to the left by 5 degrees
+%         end
+%         wallSensor =  WallSensorReadRoomba(serPort);
+
     end
 end
