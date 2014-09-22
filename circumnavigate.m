a484a595c78f569f8dafae4eda29f8388e285a1f
Alexandra Orth <orth.alexandra@gmail.com>
2014-09-22 12:30:50 -0400

    Update circumnavigate.m

--- circumnavigate.m
+++ circumnavigate.m
@@ -2,22 +2,26 @@ function finished= circumnavigate(serPort)
     forward= .2;
     angle= 0;
     finished= 0;
-    forceContact()
-    orientToWall()
-    traverseWall()
-
+    forceContact(serPort)
+    while true
+        orientToWall(serPort)
+        traverseWall(serPort)
+        turnCorner(serPort)
+    end
+        
 function forceContact(serPort)
     BumpFront = 0
     BumpRight = 0
     BumpLeft = 0
+    SetFwdVelAngVelCreate(serPort, .1, 0)
     while true 
         [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
             BumpFront] = BumpsWheelDropsSensorsRoomba(serPort)
 
         if BumpFront == 1 || BumpRight == 1 || BumpLeft == 1
+            SetFwdVelAngVelCreate(serPort, 0, 0)
             break
         end
-        SetFwdVelAngVelCreate(serPort, .1, 0)
     end
 
 function traverseWall(serPort)
@@ -31,13 +35,23 @@ function traverseWall(serPort)
 
 function orientToWall(serPort)
     wallSensor = WallSensorReadRoomba(serPort) 
-    SetFwdVelAngVelCreate(serPort, 0, 0.1)
+    SetFwdVelAngVelCreate(serPort, 0, 0.5)
     while true
         if WallSensorReadRoomba(serPort) == 1
             SetFwdVelAngVelCreate(serPort, 0, 0)
             break
         end 
-        %turnAngle(serPort, .2, 5); %turn 5 degree increments
-        pause(.5)
     end
-
+    
+function turnCorner(serPort)
+    SetFwdVelAngVelCreate(serPort, .05, -.17);
+    while true
+        [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
+            BumpFront] = BumpsWheelDropsSensorsRoomba(serPort)
+        
+        if BumpFront || BumpRight || BumpLeft
+            SetFwdVelAngVelCreate(serPort, 0, 0)
+            break
+        end 
+    end
+        
