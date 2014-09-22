64095d07866eed6872cefee3e097621a211576fb
Emily Chen <ec2805@columbia.ed>
2014-09-22 12:30:42 -0400

    stubbed out circumnavigate

--- circumnavigate.m
+++ circumnavigate.m
@@ -2,22 +2,31 @@ function finished= circumnavigate(serPort)
     forward= .2;
     angle= 0;
     finished= 0;
-    forceContact()
-    orientToWall()
-    traverseWall()
+    forceContact(serPort)
+    orientToWall(serPort)
+    traverseWall(serPort)
+    SetFwdVelAngVelCreate(serPort, 0, 0)
+    turnCorner(serPort)
+
+function turnCorner(serPort)
+    SetFwdVelAngVelCreate(serPort, 0.5, -0.2)
+    while true
+        [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
+            BumpFront] = BumpsWheelDropsSensorsRoomba(serPort)
+        if BumpFront == 1 || BumpRight == 1 || BumpLeft == 1
+            SetFwdVelAngVelCreate(serPort, 0, 0)
+            break
+        end
 
 function forceContact(serPort)
-    BumpFront = 0
-    BumpRight = 0
-    BumpLeft = 0
+    SetFwdVelAngVelCreate(serPort, .1, 0)
     while true 
         [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
             BumpFront] = BumpsWheelDropsSensorsRoomba(serPort)
-
         if BumpFront == 1 || BumpRight == 1 || BumpLeft == 1
+            SetFwdVelAngVelCreate(serPort, 0, 0)
             break
-        end
-        SetFwdVelAngVelCreate(serPort, .1, 0)
+        end 
     end
 
 function traverseWall(serPort)
@@ -31,7 +40,7 @@ function traverseWall(serPort)
 
 function orientToWall(serPort)
     wallSensor = WallSensorReadRoomba(serPort) 
-    SetFwdVelAngVelCreate(serPort, 0, 0.1)
+    SetFwdVelAngVelCreate(serPort, 0, 0.5)
     while true
         if WallSensorReadRoomba(serPort) == 1
             SetFwdVelAngVelCreate(serPort, 0, 0)
