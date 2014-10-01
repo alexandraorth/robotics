44731469196bd179158cca621e1278ada58b9252
Alexandra Orth <alexandra.orth@me.com>
2014-10-01 18:22:08 -0400

    starting hw2

--- hw1/circumnavigate.m
+++ hw1/circumnavigate.m
@@ -1,11 +1,28 @@
-function finished= circumnavigate(serPort)    
-    error = .04;
+% COMS W4733 Computational Aspects of Robotics 2014
+%
+% Homework 1
+%
+% Team number: 17
+% Team leader: Alexandra Orth (alo2117)
+% Team members: Tony Ling (tl2573) and Emily Chen (ec2805)
+% Parameters: This program does not work on the simulator, 
+%             but it does work correctly on the robot.
+
+% To run: circumnavigate(r) , where r is the robot object
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function hw1_team_17(serPort)    
+%     declare variables
+    error = .04; % the error by which we are determining when to stop 
     cumulativeAngle = 0;
     totalx = 0;
     totaly = 0;
     totaldist = 0;
     
-    num_zeros = 1;
+    num_zeros = 1; % the number of 'zero' conditions encountered. 
+                   % encountering two means you should stop the robot
     arrived = false;
   
     forceContact(serPort);
@@ -28,7 +45,12 @@ function finished= circumnavigate(serPort)
         end
     end
     
+    
+%     Modifies the x, y, and distance varibles based on the amount traveled
+%     since the last time this function was called. Checks to see if the robot 
+%     has arrived at its starting location
     function updateTravelHistory(serPort)
+%         update global variables
         cumulativeAngle = cumulativeAngle + AngleSensorRoomba(serPort);
         distance = DistanceSensorRoomba(serPort);
         totaldist = totaldist + distance;
@@ -44,21 +66,15 @@ function finished= circumnavigate(serPort)
         disp('totaly')
         disp(totaly)
         
-        disp('xpow')
-        disp(power(totalx, 2))
-        disp('ypow')
-        disp(power(totaly, 2))
-        disp('totalpow')
-        disp(power(totaldist * error, 2))
-        disp(power(totalx, 2) + power(totaly, 2) >= power(totaldist * error, 2))
-        
-        if (power(totalx, 2) + power(totaly, 2) <= power(totaldist * error, 2)) && (num_zeros >=3)
+%         check ending conditions
+        if (power(totalx, 2) + power(totaly, 2) <= power(totaldist * error, 2)) && (num_zeros >=2)
             arrived = true; 
         elseif power(totalx, 2) + power(totaly, 2) <= power(totaldist * error, 2)
             num_zeros = num_zeros + 1;
         end
     end
 
+%     Drives the robot straight until a wall is hit and then resets the odometers
     function forceContact(serPort)
         SetFwdVelAngVelCreate(serPort, .1, 0)
         while true 
@@ -79,6 +95,8 @@ function finished= circumnavigate(serPort)
     end
 
 
+%     Spins the robot until it has spun an entire circle or the robot has 
+%     found the wall. 
     function result = orientToWall(serPort)
         AngleSensorRoomba(serPort);
         totalAngle = 0;
@@ -100,11 +118,15 @@ function finished= circumnavigate(serPort)
         end
     end
 
+%     Tranverses the wall until the wall sensor is no longer activated
+%     Updates the travel history frequently
     function traverseWall(serPort)
         SetFwdVelAngVelCreate(serPort, .1, 0);
         while true
             if WallSensorReadRoomba(serPort) == 0
+                
                 updateTravelHistory(serPort);
+                
                 SetFwdVelAngVelCreate(serPort, 0, 0);
                 break
             end 
@@ -112,6 +134,8 @@ function finished= circumnavigate(serPort)
         end
     end
 
+%     Turns the corner in a clockwise circle, until a wall is found
+%     Updates the travel history frequently
     function turnCorner(serPort)
         SetFwdVelAngVelCreate(serPort, .05, -.2);
         while true
