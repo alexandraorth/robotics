1ae50a83763f248e67743a9a1a0cc737cac63725
Tony Ling <tonyling@users.noreply.github.com>
2014-10-02 19:03:52 -0400

    simulator robot now turns around obstacle
    Because of the wall sensor not being the same in the simulator as on the actual robot, the simulator robot just bumps into an obstacle, thinks its stuck in a corner, and rotate counterclockwise about 10 degrees and then turns a corner on repeat.  This does seem to move the robot around the obstacle, but need to test on actual robot if it works as intended with correct wall sensors.

--- hw2/bug2.m
+++ hw2/bug2.m
@@ -42,7 +42,7 @@ function bug2(serPort)
        if( ~finished )
            circumnavigate(serPort);
        end
-       pause(.1);
+       pause(.01);
     end
     
     
@@ -70,19 +70,60 @@ function bug2(serPort)
                 finished = true;
                 break;
             end
-            pause(.1);
+            pause(.01);
+        end
+    end
+
+    % Function rotates robot counterclockwise and returns
+    % Updates global angle as well
+    function getunstuck(serPort)
+        AngleSensorRoomba(serPort);
+        totalAngle = 0; % used to prevent robot from spinning forever
+        last_pos_angle = 0;
+        SetFwdVelAngVelCreate(serPort, 0, 0.5);
+        
+        % Rotate robot counterclockwise about 10 degrees
+        while totalAngle <= 0.175
+            
+            % have to update global angle
+            angle_change = AngleSensorRoomba(serPort)
+            angle = angle + angle_change
+            
+            %store last known postive angle value
+            if (angle_change > 0)
+                last_pos_angle = angle_change;
+            end
+            %only add to totalangle if the angle value is positive
+            if (angle_change > 0)
+                totalAngle = totalAngle + angle_change
+            else
+                totalAngle = totalAngle + last_pos_angle
+            end
+            pause(.01)
         end
     end
 
     % function travels around an object until it reaches the mline
     function circumnavigate(serPort)
-        % Once this function is entered, it is assumed that x is 0
-        found_wall = orientToWall(serPort)
-        if (found_wall == 1)
-            %traverse wall
-        else
-            %have not found wall, probably around a corner
+        while(true)
+            % Once this function is entered, it is assumed that x is 0
+            found_wall = orientToWall(serPort)
+            if (found_wall == 1)
+                %traverse wall
+                in_corner = traverseWall(serPort);
+                if (in_corner == 1) % If stuck in corner, turn at least 10 degrees
+                    getunstuck(serPort);
+                else
+                    turnCorner(serPort);
+                end
+                
+            else
+                %have not found wall, probably around a corner
+                turnCorner(serPort);
+            end
+            pause(.01)
         end
+        
         %while( x ~= 0 && contacty > y)
             %pause(.1);
            %%OLD CIRCUMNAVIGATE CODE THAT INCLUDES LOGIC TO TEST FOR
@@ -120,10 +161,27 @@ function bug2(serPort)
         result = 0;
         SetFwdVelAngVelCreate(serPort, 0, 0.5);
         while totalAngle <= 6.28
+            % Found wall, rotate left until wall is no longer found then
+            % rotate back until wall is found again
             if WallSensorReadRoomba(serPort) == 1
+                while WallSensorReadRoomba(serPort) == 1
+                    angle_change = AngleSensorRoomba(serPort);
+                    angle = angle + angle_change
+                    pause(0.01)
+                end
+                SetFwdVelAngVelCreate(serPort, 0, -0.3);
+                disp('1')
+                WallSensorReadRoomba(serPort)
+                while WallSensorReadRoomba(serPort) == 0
+                    angle_change = AngleSensorRoomba(serPort);
+                    angle = angle + angle_change
+                    pause(0.01)
+                end
                 SetFwdVelAngVelCreate(serPort, 0, 0);
                 result = 1;
-        
+                disp('2')
+                WallSensorReadRoomba(serPort)
+                
                 %update angle as well
                 angle_change = AngleSensorRoomba(serPort);
                 angle = angle + angle_change
@@ -144,10 +202,59 @@ function bug2(serPort)
             else
                 totalAngle = totalAngle + last_pos_angle
             end
-            pause(.1)
+            pause(.01)
         end
         %reset distance in case rotation counts as distance
         DistanceSensorRoomba(serPort); 
     end
+
+    % Tranverses the wall until the wall sensor is no longer activated
+    % Updates the travel history frequently
+    % Function returns when wall is no longer detected or stuck in corner
+    function in_corner = traverseWall(serPort)
+        in_corner = 0;
+        SetFwdVelAngVelCreate(serPort, .1, 0);
+        disp('3 travel')
+        WallSensorReadRoomba(serPort)
+        while true
+            [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
+            BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
+            updateYAX(serPort);
+            
+            % If in a corner
+            if WallSensorReadRoomba(serPort) == 1 && (BumpFront || BumpLeft || BumpRight)
+                SetFwdVelAngVelCreate(serPort, 0, 0);
+                updateYAX(serPort);
+                in_corner = 1
+                break
+            end
+            % If wall no longer detected, probably next to corner
+            if WallSensorReadRoomba(serPort) == 0
+                SetFwdVelAngVelCreate(serPort, 0, 0);
+                updateYAX(serPort);
+                break
+            end
+            pause(.01)
+        end
+        disp('5 corner')
+        WallSensorReadRoomba(serPort)
+    end
+
+    % function used to turn the corner
+    function turnCorner(serPort)
+    SetFwdVelAngVelCreate(serPort, .05, -.2);
+        while true
+            [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
+            BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
+            updateYAX(serPort);
+            if BumpFront || BumpRight || BumpLeft
+                SetFwdVelAngVelCreate(serPort, 0, 0)
+                updateYAX(serPort);
+                break
+            end
+            pause(.1)
+        end
+    end
+
 end
 
