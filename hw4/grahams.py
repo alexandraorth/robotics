0d8f609c23bc05ae6337cef3d6d8c04a87efb3a7
Tony Ling <tl2573@columbia.edu>
2014-11-08 13:59:14 -0500

    updated grahams.py

--- hw4/grahams.py
+++ hw4/grahams.py
@@ -1,14 +1,28 @@
-def grahams:
-	print 'this is grahams'
+import sys
+
+def grahams():
+	print('this is grahams')
 
 
 
 if __name__ == "__main__":
+	##length = 0
+	new_obs = False
+	obstacles = 0
 	try:
-		countsfile = open('obstacles.txt')
+		with open('obstacles.txt', 'r') as file:
+			for line in file:
+				if obstacles == 0:
+					obstacles = int(line)
+				elif new_obs == False:
+					length = int(line)
+					new_obs = True
+				else
+					points = []
+					
+				print(line.strip())
 
-		length 
-
+'''
 	except IOError:
 		sys.stderr.write("ERROR: Cannot read inputfile.\n")
-		sys.exit(1)
+		sys.exit(1)'''
