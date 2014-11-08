f0240c1a6aca82c4a6217f5315b10509dd523d6f
Alexandra Orth <alexandra.orth@me.com>
2014-11-08 13:43:17 -0500

    stuff in grahams

---
+++ hw4/grahams.py
@@ -0,0 +1,14 @@
+def grahams:
+	print 'this is grahams'
+
+
+
+if __name__ == "__main__":
+	try:
+		countsfile = open('obstacles.txt')
+
+		length 
+
+	except IOError:
+		sys.stderr.write("ERROR: Cannot read inputfile.\n")
+		sys.exit(1)
