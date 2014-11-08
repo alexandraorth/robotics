4420dc211c818d34dbb44bd1092131f2ff0b22d4
Alexandra Orth <alexandra.orth@me.com>
2014-11-08 14:18:45 -0500

    studd in grahams v2

--- hw4/grahams.py
+++ hw4/grahams.py
@@ -1,7 +1,8 @@
 import sys
 
-def grahams():
-	print('this is grahams')
+def grahams(obstacle):
+	print(obstacle.sort(key=lambda x: x[1]))
+	# for coordinate in obstacle:
 
 
 
@@ -11,18 +12,34 @@ if __name__ == "__main__":
 	obstacles = 0
 	try:
 		with open('obstacles.txt', 'r') as file:
-			for line in file:
-				if obstacles == 0:
-					obstacles = int(line)
-				elif new_obs == False:
-					length = int(line)
-					new_obs = True
-				else
-					points = []
-					
-				print(line.strip())
+			obstacles = int(file.readline().strip())
 
-'''
+			# create object			
+			obj = []
+			for i in range(obstacles):
+				length = int(file.readline().strip())
+				obstacle = []
+				for j in range(length):
+					line = file.readline().strip()
+					coordinate = [float(i) for i in line.split(' ')]
+					obstacle.append(coordinate)
+				obj.append(obstacle)
+
+			grahams(obj[1])
+			
+
+
+			# for line in file:
+			# 	if obstacles == 0:
+			# 		obstacles = int(line)
+			# 	elif new_obs == False:
+			# 		length = int(line)
+			# 		new_obs = True
+			# 	else
+			# 		points = []
+					
+			# 	print(line.strip())
+
 	except IOError:
 		sys.stderr.write("ERROR: Cannot read inputfile.\n")
-		sys.exit(1)'''
+		sys.exit(1)
